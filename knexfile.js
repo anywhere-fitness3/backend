c6d4bd1b5860ad6ff4e9eefe405ebf16dff489b2
Amy Barba <Amybarba70@gmail.com>
2019-12-22 19:25:16 -0800

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,37 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./dev.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    },
+    pool: {
+      afterCreate: function(connection, done) {
+        connection.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   },
 
   production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     }
   }
-
 };
