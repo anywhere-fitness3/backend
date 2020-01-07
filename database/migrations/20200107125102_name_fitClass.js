a80dee04fe5cf5e8971e784a4bb4246d69cac9f8
Amy Barba <Amybarba70@gmail.com>
2020-01-07 13:24:20 -0800

    coded users migration

--- database/migrations/20200107125102_name_fitClass.js
+++ database/migrations/20200107125102_name_fitClass.js
@@ -6,3 +6,21 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   
 };
+CREATE TABLE "Name_FitClass" (
+	"class_id" serial NOT NULL,
+	"class_name" varchar(255),
+	"class_type" varchar(255),
+	"class_duration" varchar(255),
+	"class_intensity_level" varchar(255),
+	"class_location" varchar(255),
+	"total_registered" varchar(255),
+	"max_allowed_registered" varchar(255),
+	CONSTRAINT "Name_FitClass_pk" PRIMARY KEY ("class_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+ALTER TABLE "Name_FitClass" 
+ADD CONSTRAINT "Name_FitClass_fk0" 
+FOREIGN KEY ("class_id") REFERENCES "Users"("class_id");
--- database/migrations/20200107125138_users.js
+++ database/migrations/20200107125138_users.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+      tbl.string('user_name', 256).notNullable().unique();
+      tbl.string('password').notNullable();
+      tbl.boolean('instructor').notNullable();
+      tbl.string('class_id').notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('users')
 };
+
+
+// ALTER TABLE "Users" ADD CONSTRAINT "Users_fk0" FOREIGN KEY ("class_id") REFERENCES "Users"("class_id");
+
