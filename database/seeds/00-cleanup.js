85e5448188230851f0f99808fb79cd240eb990ca
Amy Barba <Amybarba70@gmail.com>
2020-01-07 23:00:57 -0800

    troubleshooting knex cleaner error

--- database/seeds/00-cleanup.js
+++ database/seeds/00-cleanup.js
@@ -1,9 +1,13 @@
-const cleaner = require('knex-cleaner');
+const cleaner = require("knex-cleaner");
 
-exports.seed = function(knex, Promise) {
+const knex = require("../db-config.js");
+
+exports.seed = function( knex, Promise) {
   // Deletes ALL existing entries
-  return cleaner.clean(knex, {
-    mode: 'truncate', 
-    ignoreTables: ['knex_migrations', 'knex_migrations_lock']
-  });
+ 
+  return cleaner.clean( knex );
+  // return cleaner.clean( knex, {
+  //   mode: "truncate",
+  //   ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+  // });
 };
--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require('cors');
-const server = express();
 
-server.use(morgan("dev"));
-server.use(helmet());
-server.use(cors());
+const server = require("./api/server");
 
 const port = process.env.port || 5000;
 
 server.listen(port, () => {
-  console.log("Server is up and running! ");
+  console.log(`\n* Server Running on port: ${port} *\n`);
 });
