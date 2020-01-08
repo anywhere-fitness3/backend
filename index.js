b7d3ae81c9f0d66f19b8f0cb9595427c7767f851
Amy Barba <Amybarba70@gmail.com>
2020-01-08 09:22:36 -0800

    corrected path in index

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const server = require('./api/server.js');
-require('dotenv').config();
+require("dotenv").config();
 
-cont port = process.env.PORT || 9000;
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
-
-
-
-
-// require("dotenv").config();
-// const express = require("express");
-// const morgan = require("morgan");
-// const helmet = require("helmet");
-// const cors = require('cors');
-// const server = express();
-
-// server.use(morgan("dev"));
-// server.use(helmet());
-// server.use(cors());
-
-// const port = process.env.port || 5000;
-
-// server.listen(port, () => {
-//   console.log("Server is up and running! ");
-// });
