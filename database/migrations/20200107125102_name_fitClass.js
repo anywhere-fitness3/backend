1035e4e444fd48d31b5c17795532fae7563c6e10
Amy Barba <Amybarba70@gmail.com>
2020-01-08 23:15:59 -0800

    debug

--- database/migrations/20200107125102_name_fitClass.js
+++ database/migrations/20200107125102_name_fitClass.js
@@ -22,32 +22,3 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists("name_fitClass");
 };
-
-
-// exports.up = async function(knex, Promise) {
-//   await knex.schema.createTable("name_fitClass", tbl => {
-//     tbl.increments();
-//     tbl
-//       .integer("class_id")
-//       .unsigned()
-//       .notNullable()
-//       .references('id')
-//       .inTable('users')
-//       .onDelete("CASCADE")
-//       .onUpdate("CASCADE");
-//     tbl.string("class_type");
-//     tbl.string("class_name");
-//    tbl.date('class_date').defaultTo('Pending');
-//    tbl.string('class_time').defaultTo('Pending');
-//     tbl.string("class_duration").defaultTo('Pending');
-//     tbl.string('class_city').defaultTo('Pending');
-//     tbl.string('class_state').defaultTo('Pending');
-//     tbl.string("class_intensity_level");
-//     tbl.integer("total_registered");
-//     tbl.integer("max_allowed_registered");
-//   });
-// };
-
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTableIfExists("name_fitClass");
-// };
--- database/migrations/20200107185625_user_course.js
+++ database/migrations/20200107185625_user_course.js
@@ -24,28 +24,3 @@ exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists("user_course");
 };
 
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable("user_course", tbl => {
-//     tbl.increments();
-//     tbl
-//       .integer("user_id")
-//       .unsigned()
-//       .notNullable()
-//       .references('id')
-//       .inTable('users')
-//       .onDelete('CASCADE')
-//       .onUpdate('CASCADE');
-//     tbl
-//       .integer("class_id")
-//       .unsigned()
-//       .notNullable()
-//       .references('id')
-//       .inTable('name_fitClass')
-//       .onDelete("CASCADE")
-//       .onUpdate("CASCADE");
-//   });
-// };
-
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTableIfExists("user_course");
-// };
