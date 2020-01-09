a21e2b87fce7f8bb57ec697a4fff1bb1a9eb3b8e
Amy Barba <Amybarba70@gmail.com>
2020-01-08 23:08:57 -0800

    debug

--- api/middleware.js
+++ api/middleware.js
@@ -7,13 +7,3 @@ module.exports = server = => {
     server.use(express.json());
     server.use(cors());
 };
-
-// const express = require('express');
-// const helmet = require('helmet');
-// const cors = require('cors');
-
-// module.exports = server = => {
-//     server.use(helmet());
-//     server.use(express.json());
-//     server.use(cors());
-// };
