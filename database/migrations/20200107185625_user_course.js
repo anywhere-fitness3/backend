a485bfe66bb5836c7a1c51a265316aaef8ba3a29
Amy Barba <Amybarba70@gmail.com>
2020-01-08 22:00:48 -0800

    debug errors

--- database/migrations/20200107185625_user_course.js
+++ database/migrations/20200107185625_user_course.js
@@ -1,13 +1,29 @@
-exports.up = async function(knex, Promise) {
-    await knex.schema.createTable("user_course", tbl => {
-      tbl.increments("id").primary();
-      tbl
-        .integer("user_id");
-        tbl
-        .integer("class_id");
-    });
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("user_course");
-  };
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("user_course");
+};
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("user_course", tbl => {
+    tbl.increments();
+    tbl
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("class_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("name_fitClass")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("user_course");
+};
