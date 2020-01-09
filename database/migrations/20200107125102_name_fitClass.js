e366274706ec63121b1ceac0d8ce9c9036f4acab
Amy Barba <Amybarba70@gmail.com>
2020-01-08 21:22:03 -0800

    debug

--- database/migrations/20200107125102_name_fitClass.js
+++ database/migrations/20200107125102_name_fitClass.js
@@ -1,12 +1,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("name_fitClass", tbl => {
     tbl.increments();
-    // tbl
-    //   .integer("class_id")
-    //   .unsigned()
-    //   // .references('id')
-    //   .onDelete("CASCADE")
-    //   .onUpdate("CASCADE");
+    tbl
+      .integer("class_id")
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
     tbl.string("class_name", 255);
     tbl.string("class_type", 255);
     tbl.string("class_duration", 255);
@@ -14,7 +16,6 @@ exports.up = function(knex, Promise) {
     tbl.string("class_location");
     tbl.string("total_registered", 10);
     tbl.string("max_allowed_registered", 10);
-    tbl.integer("instructor_id").references('user_id').inTable('users');
   });
 };
 
